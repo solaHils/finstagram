702938c2fea18db6f143c3a6d6d7a2ee9acaebbd
solaHils <sola.hils@gmail.com>
2021-11-27 22:46:25 +0000

    W4D1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+    has_many :cutestagram_posts
+    has_many :comments
+    has_many :likes
+
+    validates :email, :username, uniqueness: true
+    validates :email, :username, :avatar_url, :password, presence: true
 
 end
